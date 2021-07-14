7eed4647bc1cd83a8b641d0ddb56316a6b05a04e
Stéphane INGRAND <79111262+StephINGRAND@users.noreply.github.com>
2021-07-14 12:02:22 +0200

    Update openvpn.py
    6eme version - suppression des variables et utilisation de texte.replace pour chaque élément

--- openvpn.py
+++ openvpn.py
@@ -16,11 +16,7 @@ def questions():
     client_crt = (open("client.crt", "r")).read()
     client_key = (open("client.key", "r")).read()
     ta_key = (open("ta.key", "r")).read()
-
-
-    
-    var1 = ["<serveur_vpn>", "<port_srv>", "<ca_cert>", "<cert_cert>", "<private_key>", "<ta_key>"]
-    var2 = [IP_serveur, Port_serveur, ca_crt, client_crt, client_key, ta_key]
+    rsa_sign = (open("rsasign.rsa", "r")).read()
 
 
 # récap de la config si oui, on génère le fichier, si non on repose les questions
@@ -28,23 +24,19 @@ def questions():
     print("\n", Nom,"\n", Prenom, "\n", IP_serveur, "\n", Port_serveur)
     Correct = input("Est-ce correct ? O/N : ")
     if Correct == "O":
-        open("{}.ovpn".format(Nom+Prenom), "w")
+        with open("client.ovpn", "r") as fichier1, open("{}.ovpn".format(Nom+Prenom), "w") as fichier2:
+            texte = fichier1.read()
+            texte = texte.replace("<serveur_vpn>", IP_serveur)
+            texte = texte.replace("<port_srv>", Port_serveur)
+            texte = texte.replace("<ca_cert>", ca_crt)
+            texte = texte.replace("<cert_cert>", client_crt)
+            texte = texte.replace("<private_key>", client_key)
+            texte = texte.replace("<ta_key>", ta_key)
+            texte = texte.replace("<rsa_sign>", rsa_sign)
+            fichier2.write(texte)
+            fichier2.close
 
-        for x in var1:
-            for y in var2:
-                
-                print("je remplace " + x + " par " + y)
-                with open("client.ovpn", "r") as fichier1, open("{}.ovpn".format(Nom+Prenom), "r+") as fichier2:
-                    texte = fichier1.read()
-#                   texte = texte.replace(x, y) vu avec Fabien !
 
-                    
-#                    fichier_modif = texte.replace(x, y)
-#                    fichier2.write(fichier_modif)
-#                    fichier2.close
-
-        
-       
             
             
     elif Correct == "N":
