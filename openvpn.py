90613417ce28f4cfee26bcb8ba8e800ae5751ae0
Stéphane INGRAND <79111262+StephINGRAND@users.noreply.github.com>
2021-07-14 12:01:44 +0200

    Update openvpn.py
    5eme version - mise en place de 2 listes pour remplacer les variables dans fichier modèle avec une seule ligne

--- openvpn.py
+++ openvpn.py
@@ -18,49 +18,33 @@ def questions():
     ta_key = (open("ta.key", "r")).read()
 
 
+    
+    var1 = ["<serveur_vpn>", "<port_srv>", "<ca_cert>", "<cert_cert>", "<private_key>", "<ta_key>"]
+    var2 = [IP_serveur, Port_serveur, ca_crt, client_crt, client_key, ta_key]
+
+
 # récap de la config si oui, on génère le fichier, si non on repose les questions
 
     print("\n", Nom,"\n", Prenom, "\n", IP_serveur, "\n", Port_serveur)
     Correct = input("Est-ce correct ? O/N : ")
     if Correct == "O":
-        with open("client.ovpn", "r") as fichier1, open("{}.ovpn".format(Nom+Prenom), "w") as fichier2:
-            texte = fichier1.read()
-            fichier_modif = texte.replace("<serveur_vpn>", IP_serveur)
-            fichier2.write(fichier_modif)
-            fichier2.close
+        open("{}.ovpn".format(Nom+Prenom), "w")
 
-        with open("{}.ovpn".format(Nom+Prenom), "r") as fichier2, open("{}.ovpn".format(Nom+Prenom), "r+") as fichier3:
-            texte2 = fichier2.read()
-            fichier2_modif = texte2.replace("<port_srv>", Port_serveur)
-            fichier3.write(fichier2_modif)
-            fichier3.close
+        for x in var1:
+            for y in var2:
+                
+                print("je remplace " + x + " par " + y)
+                with open("client.ovpn", "r") as fichier1, open("{}.ovpn".format(Nom+Prenom), "r+") as fichier2:
+                    texte = fichier1.read()
+#                   texte = texte.replace(x, y) vu avec Fabien !
 
+                    
+#                    fichier_modif = texte.replace(x, y)
+#                    fichier2.write(fichier_modif)
+#                    fichier2.close
 
-        with open("{}.ovpn".format(Nom+Prenom), "r") as fichier2, open("{}.ovpn".format(Nom+Prenom), "r+") as fichier3:
-            texte2 = fichier2.read()
-            fichier2_modif = texte2.replace("<ca_cert>", ca_crt)
-            fichier3.write(fichier2_modif)
-            fichier3.close
-
-        with open("{}.ovpn".format(Nom+Prenom), "r") as fichier2, open("{}.ovpn".format(Nom+Prenom), "r+") as fichier3:
-            texte2 = fichier2.read()
-            fichier2_modif = texte2.replace("<cert_cert>", client_crt)
-            fichier3.write(fichier2_modif)
-            fichier3.close
-
-        with open("{}.ovpn".format(Nom+Prenom), "r") as fichier2, open("{}.ovpn".format(Nom+Prenom), "r+") as fichier3:
-            texte2 = fichier2.read()
-            fichier2_modif = texte2.replace("<private_key>", client_key)
-            fichier3.write(fichier2_modif)
-            fichier3.close
-
-        with open("{}.ovpn".format(Nom+Prenom), "r") as fichier2, open("{}.ovpn".format(Nom+Prenom), "r+") as fichier3:
-            texte2 = fichier2.read()
-            fichier2_modif = texte2.replace("<ta_key>", ta_key)
-            fichier3.write(fichier2_modif)
-            fichier3.close
-
-
+        
+       
             
             
     elif Correct == "N":
